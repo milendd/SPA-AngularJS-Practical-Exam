5b3b146057d7857f51e6d56a2815574e0c9f9d8d
milendd <milen_d@abv.bg>
2015-01-03 12:41:37 +0200

    add basic listing of the ads

--- scripts/app/controllers/AdsController.js
+++ scripts/app/controllers/AdsController.js
@@ -1,7 +1,22 @@
 "use strict";
 
-app.controller('AdsController', ['$scope', 'CategoriesResource', 'TownsResource',
-    function ($scope, CategoriesResource, TownsResource) {
+app.controller('AdsController', ['$scope', 'pageSize', 'CategoriesResource', 'TownsResource', 'AdsResource',
+    function ($scope, pageSize, CategoriesResource, TownsResource, AdsResource) {
         $scope.categories = CategoriesResource.getAll();
         $scope.towns = TownsResource.getAll();
+
+        $scope.requestParameters = {
+            startPage: 1,
+            pageSize: pageSize
+        };
+
+        $scope.getAds = function (requestParameters) {
+            AdsResource.getAll(requestParameters)
+                .then(function (data) {
+                    $scope.ads = data.ads;
+                }
+            )
+        };
+
+        $scope.getAds($scope.requestParameters);
     }]);
