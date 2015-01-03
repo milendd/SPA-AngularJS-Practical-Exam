33c711e644a117ecb6844b8e8fc150567b6983de
milendd <milen_d@abv.bg>
2015-01-03 22:26:05 +0200

    finish paging functionality

--- scripts/app/controllers/AdsController.js
+++ scripts/app/controllers/AdsController.js
@@ -14,9 +14,37 @@ app.controller('AdsController', ['$scope', 'pageSize', 'CategoriesResource', 'To
             AdsResource.getAll(requestParameters)
                 .then(function (data) {
                     $scope.ads = data.ads;
+                    $scope.pagesList = new Array(data.numPages);
+                    $scope.maxPage = $scope.pagesList.length;
                 }
             )
         };
 
         $scope.getAds($scope.requestParameters);
+
+        // paging:
+        $scope.firstPage = function () {
+            $scope.requestParameters.startPage = 1;
+            $scope.getAds($scope.requestParameters);
+        };
+        $scope.previousPage = function () {
+            if ($scope.requestParameters.startPage > 1) {
+                $scope.requestParameters.startPage--;
+            }
+            $scope.getAds($scope.requestParameters);
+        };
+        $scope.setStartPage = function (currentPage) {
+            $scope.requestParameters.startPage = currentPage;
+            $scope.getAds($scope.requestParameters);
+        };
+        $scope.nextPage = function () {
+            if ($scope.requestParameters.startPage < $scope.maxPage) {
+                $scope.requestParameters.startPage++;
+            }
+            $scope.getAds($scope.requestParameters);
+        };
+        $scope.lastPage = function () {
+            $scope.requestParameters.startPage = $scope.maxPage;
+            $scope.getAds($scope.requestParameters);
+        };
     }]);
