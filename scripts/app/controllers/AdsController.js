5c443d7d10a7dd8bfd6c851e6cc83395f7e184d6
milendd <milen_d@abv.bg>
2015-01-06 08:22:53 +0200

    fix filtering and change class selected on click

--- scripts/app/controllers/AdsController.js
+++ scripts/app/controllers/AdsController.js
@@ -48,21 +48,32 @@ app.controller('AdsController', ['$scope', 'pageSize', 'AdsResource',
 
         // filter by category and town:
         $scope.getAllCategories = function () {
+            $scope.select('categories', 0, 'selected');
             delete $scope.requestParameters.categoryId;
             $scope.requestParameters.startPage = 1;
             $scope.getAds($scope.requestParameters);
         };
         $scope.getAllTowns = function () {
+            $scope.select('towns', 0, 'selected');
             delete $scope.requestParameters.townId;
             $scope.requestParameters.startPage = 1;
             $scope.getAds($scope.requestParameters);
         };
         $scope.getByCategoryId = function (id) {
+            $scope.select('categories', id, 'selected');
+            $scope.requestParameters.startPage = 1;
             $scope.requestParameters.categoryId = id;
             $scope.getAds($scope.requestParameters);
         };
         $scope.getByTownId = function (id) {
+            $scope.select('towns', id, 'selected');
+            $scope.requestParameters.startPage = 1;
             $scope.requestParameters.townId = id;
             $scope.getAds($scope.requestParameters);
+        };
+        $scope.select = function (collection, id, className) {
+            var selector = '.' + collection + ' li';
+            $(selector).removeClass(className);
+            $($(selector).get(id)).addClass(className);
         };
     }]);
