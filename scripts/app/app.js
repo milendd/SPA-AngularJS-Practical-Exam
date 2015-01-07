608116b5dc5baf501965fac90cf483fc3e94a9ce
milendd <milen_d@abv.bg>
2015-01-07 22:41:27 +0200

    extract login and register templates

--- scripts/app/app.js
+++ scripts/app/app.js
@@ -6,6 +6,12 @@ var app = angular.module('myApp', ['ngResource', 'ngRoute'])
             .when('/', {
                 templateUrl: '../../templates/advertisements.html'
             })
+            .when('/login', {
+                templateUrl: '../../templates/loginForm.html'
+            })
+            .when('/register', {
+                templateUrl: '../../templates/registerForm.html'
+            })
     })
     .constant('homeUrl', 'http://softuni-ads.azurewebsites.net/api')
     .constant('pageSize', 5);
